914d88d89cf3622f293ea658d59566271676ab65
Skunk <skunk@dreamwall.fr>
2012-05-20 16:31:02 +0200

    add local closure library

---
+++ goog/base.js
@@ -0,0 +1,10 @@
+goog = {};
+
+goog.inherits = function(childCtor, parentCtor) {
+  /** @constructor */
+  function tempCtor() {};
+  tempCtor.prototype = parentCtor.prototype;
+  childCtor.superClass_ = parentCtor.prototype;
+  childCtor.prototype = new tempCtor();
+  childCtor.prototype.constructor = childCtor;
+};
